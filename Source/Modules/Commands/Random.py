1d5e6861cbee2e492ef1bcb7cd8568c5b7d24459
LmaxplayG <lucamaxplay@gmail.com>
2022-11-13 10:42:02 +0100

    Fixed /random bug

--- Source/Modules/Commands/Random.py
+++ Source/Modules/Commands/Random.py
@@ -12,31 +12,24 @@ class Random(Commands.Cog):
     async def random_(self, ctx: Discord.ApplicationContext, min: int = 0, max: int = 100):
         await ctx.defer()
         if(max <= min):
-            await ctx.respond(embed=Embed.RANDOM_MAX_MUST_BE_ABOVE_MIN)
+            await ctx.respond(embed=await RandomEmbeds.max_must_be_greater_than_min_embed())
         roll = (secrets.randbelow(max - min) + min)
-        embed = Copy.deepcopy(Embed.RANDOM_ROLL)
-        embed.description = embed.description.replace("{NUMBER}", roll.__str__())
-        embed.set_footer(text=embed.footer.text.replace('{MIN}', min.__str__()).replace('{MAX}', max.__str__()))
 
-        await ctx.respond(embed=embed)
+        await ctx.respond(embed=await RandomEmbeds.random_embed(roll, min, max))
 
     @Commands.slash_command(description="Gets a random color", name="random-color")
     async def randomcolor(self, ctx: Discord.ApplicationContext):
         await ctx.defer()
         roll = secrets.randbelow(0x1000000)
-        embed = Copy.deepcopy(Embed.RANDOM_COLOR)
-        embed.color = Discord.Colour(value=roll)
-        embed.description = embed.description.replace("{COLOR}", hex(roll).upper().replace('0X', '#'))
+        await ctx.respond(embed=RandomEmbeds.random_color_embed(roll))
 
-        await ctx.respond(embed=embed)
-
-    @Commands.slash_command(description="Gets a random member", name="random-member")
-    @Commands.guild_only()
-    @Commands.has_permissions(kick_members=True)
-    async def randommember(self, ctx: Discord.ApplicationContext):
-        await ctx.defer()
-        roll = secrets.choice(ctx.guild.members)
-        embed = Copy.deepcopy(Embed.RANDOM_MEMBER)
-        embed.description = embed.description.replace("{MEMBER}", roll.mention)
-
-        await ctx.respond(embed=embed)
+    # @Commands.slash_command(description="Gets a random member", name="random-member")
+    # @Commands.guild_only()
+    # @Commands.has_permissions(kick_members=True)
+    # async def randommember(self, ctx: Discord.ApplicationContext):
+        # await ctx.defer()
+        # roll = secrets.choice(ctx.guild.members)
+        # embed = Copy.deepcopy(Embed.RANDOM_MEMBER)
+        # embed.description = embed.description.replace("{MEMBER}", roll.mention)
+# 
+        # await ctx.respond(embed=embed)
--- Source/Modules/Embeds/Random.py
+++ Source/Modules/Embeds/Random.py
@@ -9,3 +9,15 @@ class RandomEmbeds:
         """)
         embed.set_footer(text=f"Min: {min} | Max: {max}")
         return embed
+    
+    async def max_must_be_greater_than_min_embed():
+        embed = Discord.Embed(color=Color.ERROR, title="Error", description=f"""\
+        Max must be greater than min\
+        """)
+        return embed
+    
+    async def random_color_embed(color: int):
+        embed = Discord.Embed(color=color, title="Random Color", description=f"""\
+        You rolled a `{hex(color).upper().replace('0X', '#')}`\
+        """)
+        return embed
